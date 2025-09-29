9e708f7b0819aa0e28acabfe2b4bdd35e07751aa
HarshitNananiya <hnananiya813@rku.ac.in>
2025-09-29 22:03:21 +0530

    Add transaction form

--- AddTransactionForm.Designer.cs
+++ AddTransactionForm.Designer.cs
@@ -1,16 +1,9 @@
-﻿namespace ExpenseManager
+﻿namespace Demo
 {
     partial class AddTransactionForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,231 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.datePicker = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboCategory = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.radioIncome = new System.Windows.Forms.RadioButton();
+            this.radioExpense = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.datePicker);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.comboCategory);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.radioIncome);
+            this.groupBox1.Controls.Add(this.radioExpense);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtAmount);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtTitle);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(16, 15);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(480, 524);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add New Transaction";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnSave.FlatAppearance.BorderSize = 0;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(33, 454);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(412, 49);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save Transaction";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // datePicker
+            // 
+            this.datePicker.CalendarFont = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.datePicker.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.datePicker.Location = new System.Drawing.Point(33, 395);
+            this.datePicker.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.datePicker.Name = "datePicker";
+            this.datePicker.Size = new System.Drawing.Size(411, 32);
+            this.datePicker.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label5.Location = new System.Drawing.Point(28, 367);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 25);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Date";
+            // 
+            // comboCategory
+            // 
+            this.comboCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.comboCategory.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboCategory.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.comboCategory.ForeColor = System.Drawing.Color.White;
+            this.comboCategory.FormattingEnabled = true;
+            this.comboCategory.Location = new System.Drawing.Point(33, 310);
+            this.comboCategory.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.comboCategory.Name = "comboCategory";
+            this.comboCategory.Size = new System.Drawing.Size(411, 33);
+            this.comboCategory.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label4.Location = new System.Drawing.Point(28, 282);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Category";
+            // 
+            // radioIncome
+            // 
+            this.radioIncome.AutoSize = true;
+            this.radioIncome.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.radioIncome.Location = new System.Drawing.Point(180, 233);
+            this.radioIncome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioIncome.Name = "radioIncome";
+            this.radioIncome.Size = new System.Drawing.Size(95, 29);
+            this.radioIncome.TabIndex = 4;
+            this.radioIncome.Text = "Income";
+            this.radioIncome.UseVisualStyleBackColor = true;
+            this.radioIncome.CheckedChanged += new System.EventHandler(this.radioType_CheckedChanged);
+            // 
+            // radioExpense
+            // 
+            this.radioExpense.AutoSize = true;
+            this.radioExpense.Checked = true;
+            this.radioExpense.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.radioExpense.Location = new System.Drawing.Point(33, 233);
+            this.radioExpense.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.radioExpense.Name = "radioExpense";
+            this.radioExpense.Size = new System.Drawing.Size(102, 29);
+            this.radioExpense.TabIndex = 3;
+            this.radioExpense.TabStop = true;
+            this.radioExpense.Text = "Expense";
+            this.radioExpense.UseVisualStyleBackColor = true;
+            this.radioExpense.CheckedChanged += new System.EventHandler(this.radioType_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label3.Location = new System.Drawing.Point(28, 204);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Type";
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtAmount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtAmount.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAmount.ForeColor = System.Drawing.Color.White;
+            this.txtAmount.Location = new System.Drawing.Point(33, 153);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(411, 32);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Amount";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtTitle.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtTitle.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle.ForeColor = System.Drawing.Color.White;
+            this.txtTitle.Location = new System.Drawing.Point(33, 73);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(411, 32);
+            this.txtTitle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 44);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // AddTransactionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "AddTransactionForm";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(512, 554);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AddTransactionForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Add Transaction";
+            this.Load += new System.EventHandler(this.AddTransactionForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RadioButton radioIncome;
+        private System.Windows.Forms.RadioButton radioExpense;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboCategory;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker datePicker;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSave;
     }
 }
--- AddTransactionForm.cs
+++ AddTransactionForm.cs
@@ -1,20 +1,105 @@
-﻿using System;
+﻿using ExpenseManager;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace ExpenseManager
+namespace Demo
 {
     public partial class AddTransactionForm : Form
     {
-        public AddTransactionForm()
+        private int currentUserID;
+        private DashboardForm dashboard; // Reference to the dashboard to refresh it
+        private string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\.NET\Demo\Database1.mdf;Integrated Security=True";
+
+        public AddTransactionForm(int userId, DashboardForm dash)
         {
             InitializeComponent();
+            currentUserID = userId;
+            dashboard = dash;
+        }
+
+        private void AddTransactionForm_Load(object sender, EventArgs e)
+        {
+            // Set default transaction type and load categories
+            radioExpense.Checked = true;
+            LoadCategories("Expense");
+        }
+
+        private void LoadCategories(string type)
+        {
+            comboCategory.DataSource = null;
+            DataTable dt = new DataTable();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = "SELECT CategoryID, CategoryName FROM Categories WHERE UserID = @UserID AND CategoryType = @Type ORDER BY CategoryName";
+                using (SqlDataAdapter da = new SqlDataAdapter(query, con))
+                {
+                    da.SelectCommand.Parameters.AddWithValue("@UserID", currentUserID);
+                    da.SelectCommand.Parameters.AddWithValue("@Type", type);
+                    da.Fill(dt);
+                }
+            }
+            comboCategory.DisplayMember = "CategoryName";
+            comboCategory.ValueMember = "CategoryID";
+            comboCategory.DataSource = dt;
+        }
+
+        private void radioType_CheckedChanged(object sender, EventArgs e)
+        {
+            if (radioIncome.Checked)
+            {
+                LoadCategories("Income");
+            }
+            else
+            {
+                LoadCategories("Expense");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            // --- Validation ---
+            if (string.IsNullOrWhiteSpace(txtTitle.Text) || comboCategory.SelectedValue == null)
+            {
+                MessageBox.Show("Title and Category are required.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            if (!decimal.TryParse(txtAmount.Text, out decimal amount) || amount <= 0)
+            {
+                MessageBox.Show("Please enter a valid positive amount.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            // --- Save to Database ---
+            string transactionType = radioIncome.Checked ? "Income" : "Expense";
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                string query = @"INSERT INTO Transactions (UserID, Title, Amount, TransactionType, CategoryID, TransactionDate) 
+                                 VALUES (@UserID, @Title, @Amount, @Type, @CategoryID, @Date)";
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    cmd.Parameters.AddWithValue("@UserID", currentUserID);
+                    cmd.Parameters.AddWithValue("@Title", txtTitle.Text);
+                    cmd.Parameters.AddWithValue("@Amount", amount);
+                    cmd.Parameters.AddWithValue("@Type", transactionType);
+                    cmd.Parameters.AddWithValue("@CategoryID", comboCategory.SelectedValue);
+                    cmd.Parameters.AddWithValue("@Date", datePicker.Value.Date);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                }
+            }
+
+            MessageBox.Show("Transaction saved successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            dashboard.LoadDashboardData(); // Refresh the dashboard data
+            this.Close();
         }
     }
 }
