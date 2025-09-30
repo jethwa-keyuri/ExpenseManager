3dda2f4425557cdcedcd7e6f26e7f37677127e14
vpandya108 <vpandya108@rku.ac.in>
2025-09-30 12:10:41 +0530

    add a esigner and logic code in manage categorie

--- ManageCategoriesForm.Designer.cs
+++ ManageCategoriesForm.Designer.cs
@@ -2,15 +2,8 @@
 {
     partial class ManageCategoriesForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,18 +15,180 @@
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnAddCategory = new System.Windows.Forms.Button();
+            this.comboCategoryType = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnDeleteCategory = new System.Windows.Forms.Button();
+            this.listCategories = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAddCategory);
+            this.groupBox1.Controls.Add(this.comboCategoryType);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCategoryName);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox1.ForeColor = System.Drawing.Color.White;
+            this.groupBox1.Location = new System.Drawing.Point(544, 28);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(485, 270);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add New Category";
+            // 
+            // btnAddCategory
+            // 
+            this.btnAddCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.btnAddCategory.FlatAppearance.BorderSize = 0;
+            this.btnAddCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAddCategory.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnAddCategory.ForeColor = System.Drawing.Color.White;
+            this.btnAddCategory.Location = new System.Drawing.Point(33, 201);
+            this.btnAddCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAddCategory.Name = "btnAddCategory";
+            this.btnAddCategory.Size = new System.Drawing.Size(421, 43);
+            this.btnAddCategory.TabIndex = 4;
+            this.btnAddCategory.Text = "Add Category";
+            this.btnAddCategory.UseVisualStyleBackColor = false;
+            this.btnAddCategory.Click += new System.EventHandler(this.btnAddCategory_Click_1);
+            // 
+            // comboCategoryType
+            // 
+            this.comboCategoryType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.comboCategoryType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboCategoryType.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.comboCategoryType.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.comboCategoryType.ForeColor = System.Drawing.Color.White;
+            this.comboCategoryType.FormattingEnabled = true;
+            this.comboCategoryType.Items.AddRange(new object[] {
+     "Expense",
+     "Income"});
+            this.comboCategoryType.Location = new System.Drawing.Point(33, 144);
+            this.comboCategoryType.Margin = new System.Windows.Forms.Padding(4);
+            this.comboCategoryType.Name = "comboCategoryType";
+            this.comboCategoryType.Size = new System.Drawing.Size(420, 33);
+            this.comboCategoryType.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label2.Location = new System.Drawing.Point(28, 116);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Category Type";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.txtCategoryName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCategoryName.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.txtCategoryName.ForeColor = System.Drawing.Color.White;
+            this.txtCategoryName.Location = new System.Drawing.Point(33, 66);
+            this.txtCategoryName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(421, 32);
+            this.txtCategoryName.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.label1.Location = new System.Drawing.Point(28, 38);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category Name";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDeleteCategory);
+            this.groupBox2.Controls.Add(this.listCategories);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.groupBox2.ForeColor = System.Drawing.Color.White;
+            this.groupBox2.Location = new System.Drawing.Point(16, 28);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(505, 647);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Existing Categories";
+            // 
+            // btnDeleteCategory
+            // 
+            this.btnDeleteCategory.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(57)))), ((int)(((byte)(43)))));
+            this.btnDeleteCategory.FlatAppearance.BorderSize = 0;
+            this.btnDeleteCategory.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDeleteCategory.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold);
+            this.btnDeleteCategory.ForeColor = System.Drawing.Color.White;
+            this.btnDeleteCategory.Location = new System.Drawing.Point(24, 583);
+            this.btnDeleteCategory.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDeleteCategory.Name = "btnDeleteCategory";
+            this.btnDeleteCategory.Size = new System.Drawing.Size(456, 43);
+            this.btnDeleteCategory.TabIndex = 1;
+            this.btnDeleteCategory.Text = "Delete Selected";
+            this.btnDeleteCategory.UseVisualStyleBackColor = false;
+            this.btnDeleteCategory.Click += new System.EventHandler(this.btnDeleteCategory_Click);
+            // 
+            // listCategories
+            // 
+            this.listCategories.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(45)))), ((int)(((byte)(48)))));
+            this.listCategories.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.listCategories.Font = new System.Drawing.Font("Segoe UI", 11.25F);
+            this.listCategories.ForeColor = System.Drawing.Color.White;
+            this.listCategories.FormattingEnabled = true;
+            this.listCategories.ItemHeight = 25;
+            this.listCategories.Location = new System.Drawing.Point(24, 38);
+            this.listCategories.Margin = new System.Windows.Forms.Padding(4);
+            this.listCategories.Name = "listCategories";
+            this.listCategories.Size = new System.Drawing.Size(455, 502);
+            this.listCategories.TabIndex = 0;
+            // 
+            // ManageCategoriesForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ManageCategoriesForm";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(1045, 690);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "ManageCategoriesForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Manage Categories";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.ComboBox comboCategoryType;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAddCategory;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ListBox listCategories;
+        private System.Windows.Forms.Button btnDeleteCategory;
     }
 }
--- ManageCategoriesForm.cs
+++ ManageCategoriesForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,122 @@ namespace ExpenseManager
 {
     public partial class ManageCategoriesForm : Form
     {
-        public ManageCategoriesForm()
+        private readonly string connectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=E:\.NET\Demo\Database1.mdf;Integrated Security=True";
+        private readonly int currentUserId;
+
+        public ManageCategoriesForm(int userId)
         {
             InitializeComponent();
+            this.currentUserId = userId;
+
+            // --- THIS IS THE FIX ---
+            // This line connects the form's Load event to your method.
+            this.Load += new System.EventHandler(this.ManageCategoriesForm_Load);
+            // --- END OF FIX ---
+        }
+
+        private void ManageCategoriesForm_Load(object sender, EventArgs e)
+        {
+            LoadCategories();
+            comboCategoryType.SelectedIndex = 0; // Default to "Expense"
+        }
+
+        private void LoadCategories()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    string query = "SELECT CategoryID, (CategoryName + ' (' + CategoryType + ')') AS DisplayName FROM Categories WHERE UserID = @UserID ORDER BY CategoryType, CategoryName";
+                    SqlDataAdapter da = new SqlDataAdapter(query, con);
+                    da.SelectCommand.Parameters.AddWithValue("@UserID", this.currentUserId);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+
+                    listCategories.DataSource = dt;
+                    listCategories.DisplayMember = "DisplayName";
+                    listCategories.ValueMember = "CategoryID";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading categories: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+        private void btnAddCategory_Click_1(object sender, EventArgs e)
+        {
+            string categoryName = txtCategoryName.Text.Trim();
+            string categoryType = comboCategoryType.SelectedItem.ToString();
+
+            if (string.IsNullOrEmpty(categoryName))
+            {
+                MessageBox.Show("Please enter a category name.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    string query = "INSERT INTO Categories (UserID, CategoryName, CategoryType) VALUES (@UserID, @CategoryName, @CategoryType)";
+                    using (SqlCommand cmd = new SqlCommand(query, con))
+                    {
+                        cmd.Parameters.AddWithValue("@UserID", this.currentUserId);
+                        cmd.Parameters.AddWithValue("@CategoryName", categoryName);
+                        cmd.Parameters.AddWithValue("@CategoryType", categoryType);
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                MessageBox.Show("Category added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtCategoryName.Clear();
+                LoadCategories(); // Refresh the list
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error adding category: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDeleteCategory_Click(object sender, EventArgs e)
+        {
+
+            if (listCategories.SelectedItem == null)
+            {
+                MessageBox.Show("Please select a category to delete.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            DialogResult confirmation = MessageBox.Show("Are you sure you want to delete this category? This may affect existing transactions.", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (confirmation == DialogResult.Yes)
+            {
+                try
+                {
+                    int categoryId = (int)listCategories.SelectedValue;
+                    using (SqlConnection con = new SqlConnection(connectionString))
+                    {
+                        con.Open();
+                        // Note: A more robust application might prevent deleting categories that are in use.
+                        // For this project, we will allow it, but show a warning.
+                        string query = "DELETE FROM Categories WHERE CategoryID = @CategoryID AND UserID = @UserID";
+                        using (SqlCommand cmd = new SqlCommand(query, con))
+                        {
+                            cmd.Parameters.AddWithValue("@CategoryID", categoryId);
+                            cmd.Parameters.AddWithValue("@UserID", this.currentUserId);
+                            cmd.ExecuteNonQuery();
+                        }
+                    }
+                    MessageBox.Show("Category deleted successfully.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    LoadCategories(); // Refresh the list
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error deleting category: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
